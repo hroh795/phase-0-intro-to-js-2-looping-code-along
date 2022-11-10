7f507b7f2918009a347009d5ca19fe92dd001d21
Ha Young Roh <hyroh795@gmail.com>
2022-11-09 21:27:08 -0500

    commit tree 67081b3d55725fc4531414cc1fda8c9a7a222089

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(arrayName, event) {
+    const newArray = [];
+    for(let i =0 ; i <arrayName.length; i++) {
+        newArray.push(`Thank you, ${arrayName[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return newArray;
+}
+
+
+function countDown(num) {
+    let count = num;
+    while (count>=0) {
+        console.log(count);
+        debugger;
+        count--;
+    };
+}
